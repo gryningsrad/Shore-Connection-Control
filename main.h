1acd0bec1872818cedc30958fcbcfc15abbcf051
Mikael Mattsson <35922880+gryningsrad@users.noreply.github.com>
2018-09-13 12:12:06 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,24 @@
+/* ##### main.h #####
+
+
+
+*/
+
+#IFNDEF main.h
+#DEFINE 
+
+// enum for the different input combinations
+/*
+
+          bit:  7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+ShoPow_Avail    x   x   x   x   x   x   x   1
+Batt_Avail      x   x   x   x   x   x   1   x
+SwtBatt         x   x   x   x   x   1   x   x
+SwtShoPow       x   x   x   x   1   x   x   x
+
+
+*/
+enum
+
+
+#ENDIF
