3a73fdb3853a654fa2612d595e2f03a51163ea96
Mikael Mattsson <35922880+gryningsrad@users.noreply.github.com>
2018-09-13 18:54:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,6 @@ E-mail: mikael@mikaelmattsson.com
 #include "main.h"
 
 // Constants / settings
-int SP_ON_DELAY = 10; // no of seconds before Shorepower is deemed stable
 int AUTO_SWITCHPOW_SHORE = 1; // Switch automatically to shore power if available READ FROM JUMPER?
 int AUTO_SWITCHPOW_BATT = 0; // Switch automatically to shore power if available READ FROM JUMPER?
 
@@ -17,85 +16,107 @@ int AUTO_SWITCHPOW_BATT = 0; // Switch automatically to shore power if available
 bool bShorePowerAvailable;
 bool bBatteriesAvailable;
 int intSwitch; // 0=off (never used), 1=batteries, 2=shore power
-int intCurrMode; // stores current stable state of operating mode, b01 = batteries, b10 = shore power
+int intCurrMode; // stores current stable state of operating mode
 
-// If shorepower-pin status changes
-interruptShorePowerchange
+// If inputpins status changes, PCINT1, pins 23-28,1, PC0-PC6
+ISR (PCINT1_vect)
 {
-  bool bNewStaus = [INPUTPIN OF SHOREPOWER SUPERVISION]
-  if(bShorePowerAvailable) // ShorePower where connected erlier ==Z Shorepower is now lost
-  {
-     bShorePowerAvailable = false;
-     RaiseAlarm(); // if alarm is set to sound, raise alarm
-     SwitchToBatteries(); // Procedure to change to battery
-     
-  } else { // Shorepower was disconnected ==Z ShorePower now connected
+  int new_status = PORTC; // read whole byte
+  _delay(300); // should take care of debouncing
   
-    // ## Time loop, 10 seconds to see that shore power connection is stable. delay defined as a constatnt ##
-    bShorePowerAvailale = true;
-    led_on(ledShorePowerAvailable);
-    
-    // If selector is set to "Shore power" = then connect
-    if (intSwitch = SWITCH_SHOREPOW && AUTO_SHOREPOW)
-    {
-      SwitchToShorePower();
-    {
+  if (new_status != PORTC) { // if we still have bouncing than...
+    _delay_ms(200); // ... add yet another 200 ms delay
   }
+  
+  if (new_status != PORTC) {
+    // TODO: Exit ISR 
+  }
+  
+  // If original state was Batteries ON
+  if (RELAY_BATT_PORT &= (1 ** RELAY_BATT_PIN))
+  {
+    // what has changed ?
+    select case (intCurrStatus ~ new_status)
+      case 1: // selector -* battery
+    
+    case 2: // selector -* shorepower
+    
+    case 4: // battery -* available NOT POSSIBLE!
+    
+    case 8: // shorepower available
+  }
+  
 }
 
-// If selector button changes 
-// 2-pins: b00 = off, b01 = batteries, b10 = shore power
-interrupSelectorChange()
+void main()
 {
-  // dependin on new value
-  switch([SELECTOT_BITES_VALUE]
-  case(b00) // off never used
+  // TODO: initalize registrers and timers
+  /*
+  
+  PCINT1 interrupt
+  CTC mode for 
+  
+  */
   
   
-  case(b10) // batteries
+  // TODO read inputs and decide which mode to set initially
   
   
-  case (b01) // shorepower
-    
+  // enable interrupts
+  
   
 }
 
 // Functino to switch from Shore Power to Batteries
 SwitchToBatteries()
 {
-  relayBatt(on);
+  // Toggle
+  relayBatt(1);
+  _delay(500);
+  relayShorePow(0);
 }
 
 // Function to switch from Batteries to Shore Power
 SwitchToShorePower()
 {
-  // Check to see if Shorepower is available
-  if (bShorePower Available)
-  {
-    relaySHPO(true);
-    delay(5); // 5 seconds
-      if (bShorePowerAvailable) // if still OK
-      {
-        relayBATT(off);
-      }
-  }
+    relayShorePow(1);
+    delay(5000); // 5 seconds
+    if (bShorePowerAvailable) // if still OK
+    {
+      relayBATT(0);
+    }
 }
 
 // Control of relayShorePower
-void relaySHPO()
+void relayShorePow(int _status)
 {
   // Set relay port to high
   
-  // Set status LED to "on"
   
+  // Set status LED to "on"
+  statusLEDShorePow(1);
+  statusLEDBatt(0);
 }
 
 // Control of relayBattery
-void relayBATT()
+void relayBATT(int _status)
 {
   // Set relay port to high
   
-  // Set status LED to "on"
   
+  // Set status LED to "on"
+  statusLEDBatt(1);
+  statusLEDShorePow(0);
 }
+
+void statusLEDShorePow(int _status) // 0 = off, 1 = on, 2 = blink
+{
+  LED_SHORE_PORT |= (1 ?? LED_SHORE_PIN);
+}
+
+void statusLEDBatt(int _status) // 0 = off, 1 = on, 2 = blink
+{
+  LED_BATT_PORT |= (1 ?? LED_BATT_PIN);
+}
+
 
