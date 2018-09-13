457e9875c0b6d7fe483cc239fd5056b8d5fad043
Mikael Mattsson <35922880+gryningsrad@users.noreply.github.com>
2018-09-13 18:09:32 +0200

    Update main.h

--- main.h
+++ main.h
@@ -7,6 +7,42 @@
 #IFNDEF main.h
 #DEFINE 
 
+
+// Output Pins
+#define LED_BATT_PORT         PORTA
+#define LED_BATT_PIN          3
+
+#DEFINE LED_SHOPOW_PORT       PORTA
+#DEFINE LED_SHOPOW_PIN        4
+
+#DEFINE RELAY_BATT_PORT       PORTA
+#DEFINE RELAY_BATT_PIN        5
+
+#DEFINE RELAY_SHOPOW_PORT     PORTA
+#DEFINE RELAY_SHOPOW_PIN      4
+
+#DEFINE BUZZER_PORT           PORTA
+#DEFINE BUZZER_PIN            7
+
+// Input pins
+#DEFINE SHOPOW_AVAIL_PORT       PORTA
+#DEFINE SHOPOW_AVAIL_PIN        4
+
+#DEFINE BATT_AVAIL_PORT       PORTA
+#DEFINE BATT_AVAIL_PIN        5
+
+#DEFINE SHOPOW_SELECT_PORT       PORTA
+#DEFINE SHOPOW_SELECT_PIN        4
+
+#DEFINE BATT_SELECT_PORT       PORTA
+#DEFINE BATT_SELECT_PIN        5
+
+// Delays and timers
+#DEFINE TIME_DELAY_BATT2SHORE 500 // in ms
+#DEFINE TIME_DELAY_SHORE2BATT 500 // in ms
+#DEFINE ALARM_SUMMER_TIME     1200
+
+
 // enum for the different input combinations
 /*
           bit:  7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
